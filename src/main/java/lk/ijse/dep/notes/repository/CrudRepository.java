5bc39b0e186ba046afefbe6aba1a9d5a68f9f6aa
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-22 22:50:50 +0530

    Finalize the CrudRepository

--- src/main/java/lk/ijse/dep/notes/repository/CrudRepository.java
+++ src/main/java/lk/ijse/dep/notes/repository/CrudRepository.java
@@ -1,6 +1,20 @@
 package lk.ijse.dep.notes.repository;
 
-import java.io.Serializable;
 
-public interface CrudRepository<T,ID extends Serializable> extends SuperRepository {
+import lk.ijse.dep.notes.entity.SuperEntity;
+
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.List;
+
+public interface CrudRepository<T extends SuperEntity,ID extends Serializable> extends SuperRepository {
+
+    ID save(T entity);
+    void deleteById(ID pk);
+    boolean existById(ID pk);
+    Optional<T>findById(ID pk);
+    List<T> findAll();
+    long count();
+
 }
