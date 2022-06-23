08df6ccaad1267fad292154cc10ddaaf08a1159a
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-23 14:49:27 +0530

    Apply the basic structure on api layer part -2

--- src/main/java/lk/ijse/dep/note/api/UserController.java
+++ src/main/java/lk/ijse/dep/note/api/UserController.java
@@ -2,9 +2,12 @@ package lk.ijse.dep.note.api;
 
 import lk.ijse.dep.note.dto.UserDTO;
 import lk.ijse.dep.note.service.UserService;
+import lk.ijse.dep.note.service.exception.DuplicateEmailException;
+import lk.ijse.dep.note.service.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -17,22 +20,40 @@ public class UserController {
     public UserDTO registerUser(@RequestBody UserDTO user){
         //validate
 
-       return user;
+
+        try {
+            userService.registerUser(user);
+        } catch (DuplicateEmailException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"Email already exists",e);
+        }
+        return user;
     }
-    @GetMapping(path = "{userId}",produces = "application/json")
+    @GetMapping(path = "{userId:[A-Fa-f0-\\~]{36}}}",produces = "application/json")
     public UserDTO getUserInfo(@PathVariable String userId){
         System.out.println("get"+userId);
-        return null;
+        try {
+            return userService.getUserInfo(userId);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(404,"invalid user id",e);
+        }
     }
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("{userId}")
+    @DeleteMapping("{userId:[A-Fa-f0-\\~]{36}}")
     public void deleteuser(@PathVariable String userId){
-        System.out.println("delete");
+        try {
+            userService.deleteUser(userId);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(404,"invalid user id",e);
+        }
     }
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping(path = "{userId}",consumes = "application/json")
+    @PatchMapping(path = "{userId:[A-Fa-f0-\\~]{36}}",consumes = "application/json")
     public void updateUser(@PathVariable String userId,@RequestBody UserDTO user){
-        System.out.println("update");
+        try {
+            userService.updateUser(user);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(404,"invalid user id",e);
+        }
     }
 
 }
